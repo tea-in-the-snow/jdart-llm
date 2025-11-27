0c617d7307388d27c96d78c58a93425293b5bf98
shaoran <xiashaoran@outlook.com.com>
2025-11-27 12:21:37 +0800

    clean it up

--- src/main/gov/nasa/jpf/jdart/bytecode/INSTANCEOF.java
+++ src/main/gov/nasa/jpf/jdart/bytecode/INSTANCEOF.java
@@ -1,15 +1,11 @@
 package gov.nasa.jpf.jdart.bytecode;
 
-import gov.nasa.jpf.vm.ElementInfo;
 import gov.nasa.jpf.vm.Instruction;
 import gov.nasa.jpf.vm.MJIEnv;
 import gov.nasa.jpf.vm.StackFrame;
 import gov.nasa.jpf.vm.ThreadInfo;
 import gov.nasa.jpf.constraints.api.Expression;
 import gov.nasa.jpf.constraints.expressions.InstanceofExpression;
-import gov.nasa.jpf.constraints.types.ConcreteType;
-import gov.nasa.jpf.constraints.types.Type;
-import gov.nasa.jpf.jdart.ConcolicUtil;
 
 public class INSTANCEOF extends gov.nasa.jpf.jvm.bytecode.INSTANCEOF {
 
@@ -24,17 +20,17 @@ public class INSTANCEOF extends gov.nasa.jpf.jvm.bytecode.INSTANCEOF {
     Object opAttr = sf.getOperandAttr();
 
     if (opAttr == null) {
-      System.out.println("**********************************************************");
-      System.out.println("Execute not symbolic INSTANCEOF. " + sf.getOperandAttr());
-      System.out.println("**********************************************************");
+      // System.out.println("**********************************************************");
+      // System.out.println("Execute not symbolic INSTANCEOF. " + sf.getOperandAttr());
+      // System.out.println("**********************************************************");
       return super.execute(ti);
     }
 
     // the condition is symbolic
 
-    System.out.println("**********************************************************");
-    System.out.println("Execute symbolic INSTANCEOF. " + opAttr);
-    System.out.println(opAttr + " instanceof " + super.getType());
+    // System.out.println("**********************************************************");
+    // System.out.println("Execute symbolic INSTANCEOF. " + opAttr);
+    // System.out.println(opAttr + " instanceof " + super.getType());
 
     int objRef = sf.pop();
 
@@ -46,12 +42,12 @@ public class INSTANCEOF extends gov.nasa.jpf.jvm.bytecode.INSTANCEOF {
 
     Expression<?> symRef = (Expression<?>) opAttr;
     Expression<?> symbExpr = new InstanceofExpression(symRef, super.getType());
-    System.out.println("Created symbolic instanceof expression: " + symbExpr.toString());
+    // System.out.println("Created symbolic instanceof expression: " + symbExpr.toString());
     
     
     sf.setOperandAttr(symbExpr);
 
-    System.out.println("**********************************************************");
+    // System.out.println("**********************************************************");
 
     return getNext(ti);
   }
